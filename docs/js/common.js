7df26353201902f163db2fababd973b245a092ac
str3lok <kostprom@gmail.com>
2020-12-18 12:19:06 +0600

    files
    files

---
+++ docs/js/common.js
@@ -0,0 +1,18 @@
+(function() {
+
+ 
+}); //- end 
+
+try {
+ $(window).scroll(function() { 
+  var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+
+  if (top > 60) { 
+   $('.header__section').addClass('is-fixed');
+   $('.wrapper__section').addClass('is-content-fixed');
+  }else{
+   $('.header__section').removeClass('is-fixed');
+   $('.wrapper__section').removeClass('is-content-fixed');
+  }
+ }); 
+} catch (e) {} 
